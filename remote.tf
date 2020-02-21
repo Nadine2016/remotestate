5aa8c6fbd2a717a0f4f87e06f66171d650268588
Nadine2016 <nspies@us.ibm.com>
2020-02-21 13:55:46 -0500

    Create remote.tf

---
+++ remote.tf
@@ -0,0 +1,15 @@
+data "ibm_schematics_workspace" "vpc" {
+  workspace_id = "vpc-9c76acd7-3585-41"
+}
+
+data "ibm_schematics_output" "vpc" {
+  workspace_id = "vpc-9c76acd7-3585-41"
+  template_id  = "${data.ibm_schematics_workspace.vpc.template_id.0}"
+}
+
+output "Output_vars" {
+  value = "${data.ibm_schematics_output.vpc.output_values}"
+}
+output "remotestate_floating_ip" {
+  value = "${data.ibm_schematics_output.vpc.output_values.floating_ip}"
+}
