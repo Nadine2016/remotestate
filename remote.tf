f7e7230ed62acd3201e148cc79a0afad4d44ebdc
Nadine2016 <nspies@us.ibm.com>
2020-04-07 07:54:16 -0400

    Update remote.tf

--- remote.tf
+++ remote.tf
@@ -1,7 +1,3 @@
-data "ibm_schematics_workspace" "vpc" {
-  workspace_id = "vpc-9c76acd7-3585-41"
-}
-
 data "ibm_schematics_output" "vpc" {
   workspace_id = "vpc-9c76acd7-3585-41"
   template_id  = "${data.ibm_schematics_workspace.vpc.template_id.0}"
